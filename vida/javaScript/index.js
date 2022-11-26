96e9b900ff5249c9c82579116e2a7cada2e954de
Javier Suarez <javiersuarez800@gmail.com>
2022-11-26 09:24:41 -0500

    Version inicial

---
+++ vida/javaScript/index.js
@@ -0,0 +1,13 @@
+function login(){ 
+    console.log("Entro a la funcion js");
+    var done=0; 
+    var usuario=document.getElementById('usuario').value;
+    var password=document.getElementById('password').value; 
+    console.log(usuario + "Contraseña: "+password);
+    if (usuario=="FELIPE" && password=="FELIPE#2022") { 
+    window.location="./Principal.html"; 
+    }   
+    else{ 
+    alert("Contraseña incorrecta"); 
+    } 
+    } 
